37d361b59dc3916379ee199701bcb35f14ae1ac6
tomas <72792707+tomas-land@users.noreply.github.com>
2021-11-03 10:38:09 +0200

    readme update

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "Tomas");
-define("DB_PASS", "Kcn92*/?dd72");
+define("DB_PASS", "********");
 define("DB_NAME", "eshop");
 
 define("SITE_TITLE", "TechShop");
